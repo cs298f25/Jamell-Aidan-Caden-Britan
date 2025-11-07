92c87c7c118fc1310e77761574e1015ba97cab5d
Jamell Alvarez <alvarezj@moravian.edu>
2025-11-07 15:57:21 -0500

    Modified global s3 client for workflow to run

--- src/storageAws.py
+++ src/storageAws.py
@@ -1,3 +1,4 @@
+# src/storageAws.py
 import boto3
 from botocore.exceptions import ClientError
 from boto3.exceptions import S3UploadFailedError
@@ -6,9 +7,14 @@ import dotenv
 
 
 dotenv.load_dotenv()
-s3 = boto3.client("s3", region_name=os.getenv('REGION'))
+REGION = os.getenv('REGION')
+
+def get_client():
+    """Helper function to create a client."""
+    return boto3.client("s3", region_name=REGION)
 
 def create_bucket(bucket_name):
+    s3 = get_client()
     try:
         s3.create_bucket(Bucket=bucket_name)
         return True
@@ -16,6 +22,7 @@ def create_bucket(bucket_name):
         return False
 
 def delete_bucket(bucket_name):
+    s3 = get_client()
     try:
         s3.delete_bucket(Bucket=bucket_name)
         return True
@@ -23,6 +30,7 @@ def delete_bucket(bucket_name):
         return False
 
 def upload_image(bucket_name, file_path, object_name=None):
+    s3 = get_client()
     if object_name is None:
         object_name = os.path.basename(file_path)
     try:
@@ -32,6 +40,7 @@ def upload_image(bucket_name, file_path, object_name=None):
         return False
 
 def list_images(bucket_name):
+    s3 = get_client()
     try:
         response = s3.list_objects_v2(Bucket=bucket_name)
         return [obj['Key'] for obj in response.get('Contents', [])]
@@ -39,6 +48,7 @@ def list_images(bucket_name):
         return []
 
 def delete_image(bucket_name, object_name):
+    s3 = get_client()
     try:
         s3.delete_object(Bucket=bucket_name, Key=object_name)
         return True
