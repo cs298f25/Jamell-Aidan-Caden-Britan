725db18d46e9acbf734d3e5a822fe67560a265a8
Aidan Casey <caseya@moravian.edu>
2025-12-01 11:10:17 -0500

    Added password functionality for login & database

--- src/static/login/script.js
+++ src/static/login/script.js
@@ -1,15 +1,17 @@
 const loginButton = document.getElementById('login-btn');
 const usernameInput = document.getElementById('username-input');
+const passwordInput = document.getElementById('password-input');
 
 // Handle login button click
 loginButton.addEventListener('click', function() {
     const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
 
-    // Check if username is empty
-    if (!username) {
-        alert('Username is required');
+    // Check if username or password is empty
+    if (!username || !password) {
+        alert('Username and password are required');
         return;
     }
-    // Go to auth page with username
-    window.location.href = `/auth?username=${encodeURIComponent(username)}`;
+    // Go to auth page with username and password
+    window.location.href = `/auth?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
 });
